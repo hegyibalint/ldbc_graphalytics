ec6b7985c346894868ad8eacd4491846ac9928d8
Gabor Szarnyas <szarnyasg@gmail.com>
2019-09-26 19:00:09 +0000

    Add stacktrace to validation error

--- graphalytics-core/src/main/java/science/atlarge/graphalytics/execution/RunnerService.java
+++ graphalytics-core/src/main/java/science/atlarge/graphalytics/execution/RunnerService.java
@@ -140,6 +140,7 @@ public class RunnerService extends MircoService {
                 runner.startup(runSpecification);
             } catch (Exception e) {
                 LOG.error("Failed to startup benchmark run.", e);
+		LOG.error(e);
                 reportFailure(BenchmarkFailure.INI);
                 throw new GraphalyticsExecutionException("Benchmark run aborted.", e);
             }
